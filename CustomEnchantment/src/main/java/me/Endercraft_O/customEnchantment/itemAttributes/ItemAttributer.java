6afb7e17b7b17a93528a4e97d970ceac98aa5c1e
EndercraftO <lui.bill921@gmail.com>
2019-08-19 23:57:03 +0800

    test

---
+++ CustomEnchantment/src/main/java/me/Endercraft_O/customEnchantment/itemAttributes/ItemAttributer.java
@@ -0,0 +1,10 @@
+package me.Endercraft_O.customEnchantment.itemAttributes;
+
+import org.bukkit.ChatColor;
+
+public class ItemAttributer {
+
+	private final static String customAttributeChar = ""+ChatColor.DARK_RED+ChatColor.DARK_AQUA+ChatColor.DARK_RED+ChatColor.DARK_BLUE+ChatColor.GRAY;
+	
+	
+}
